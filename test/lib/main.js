c8eeecafef792b7952eada3bfb64f81ff581a84d
nguyentruongsinh0311 <31197483+nguyentruongsinh0311@users.noreply.github.com>
2017-11-22 09:36:35 +0700

    submit file test

---
+++ test/lib/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	// animateOut: 'slideOutDown',
+        // animateIn: 'flipInX',
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        freeDrag: false,
+        stagePadding: 0,
+        loop: true,
+        items:1,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        autoplay: true,
+        autoplayTimeout: 3000,
+    });
+});
